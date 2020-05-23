7d5f4f4603550d303f3103dcffd4fe62a606dab0
Anthony Rota <28838817+anthonyrota@users.noreply.github.com>
2020-05-24 00:10:56 +1000

    Add extra info to ScheduleSyncQueued test comment

--- test/schedule.test.ts
+++ test/schedule.test.ts
@@ -142,8 +142,8 @@ describe('ScheduleSyncQueued', () => {
         scheduleSyncQueued(main2);
         /* eslint-enable @typescript-eslint/camelcase */
         // start execution: main1
-        // (1) main1();   [main_1, main1_2]
-        // (2) main1_1(); [main1_2, main1_3]
+        // (1) main1();   (queue): [main_1, main1_2]
+        // (2) main1_1(); (queue): [main1_2, main1_3]
         // (3) main1_2(); (4) main1_3();
         // end execution: main1
         expect(icof1(main1)).toEqual([1]);
@@ -152,26 +152,26 @@ describe('ScheduleSyncQueued', () => {
         expect(icof1(main1_3)).toEqual([4]);
         /* eslint-disable max-len */
         // start execution: main2
-        // (5) main2();   [main2, main2_1]
-        // (6) main2();   [main2_1, main2]
-        // (7) main2_1(); [main2, main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
-        // (8) main2();   [main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4, main2]
-        // (9) main2_1_1();  (10) main2_1_2(); (11) main2_1(); [main2_1_3, main2_1_4, main2, main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
-        // (12) main2_1_3(); (13) main2_1_4(); (14) main2();   [main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4, main2]
-        // (15) main2_1_1(); (16) main2_1_2(); (17) main2_1(); [main2_1_3, main2_1_4, main2, main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
-        // (18) main2_1_3(); (19) main2_1_4(); (20) main2();   [main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4, main2]
-        // (21) main2_1_1(); (22) main2_1_2(); (23) main2_1(); [main2_1_3, main2_1_4, main2, main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
-        // (24) main2_1_3(); (25) main2_1_4(); (26) main2();   [main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4, main2]
-        // (27) main2_1_1(); (28) main2_1_2(); (29) main2_1(); [main2_1_3, main2_1_4, main2, main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
-        // (30) main2_1_3(); (31) main2_1_4(); (32) main2();   [main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4, main2]
-        // (33) main2_1_1(); (34) main2_1_2(); (35) main2_1(); [main2_1_3, main2_1_4, main2, main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
-        // (36) main2_1_3(); (37) main2_1_4(); (38) main2();   [main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4, main2]
-        // (39) main2_1_1(); (40) main2_1_2(); (41) main2_1(); [main2_1_3, main2_1_4, main2, main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
-        // (42) main2_1_3(); (43) main2_1_4(); (44) main2();   [main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4, main2]
-        // (45) main2_1_1(); (46) main2_1_2(); (47) main2_1(); [main2_1_3, main2_1_4, main2, main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
-        // (48) main2_1_3(); (49) main2_1_4(); (50) main2();   [main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
-        // (51) main2_1_1(); (52) main2_1_2(); (53) main2_1(); [main2_1_3, main2_1_4, main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
-        // (54) main2_1_3(); (55) main2_1_4(); (56) main2_1_1(); (57) main2_1_2(); (58) main2_1(); [main2_1_3, main2_1_4, main2_1_1, main2_1_2, main2_1_3, main2_1_4]
+        // (5) main2();   (queue): [main2, main2_1]
+        // (6) main2();   (queue): [main2_1, main2]
+        // (7) main2_1(); (queue): [main2, main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
+        // (8) main2();   (queue): [main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4, main2]
+        // (9) main2_1_1();  (10) main2_1_2(); (11) main2_1(); (queue): [main2_1_3, main2_1_4, main2, main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
+        // (12) main2_1_3(); (13) main2_1_4(); (14) main2();   (queue): [main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4, main2]
+        // (15) main2_1_1(); (16) main2_1_2(); (17) main2_1(); (queue): [main2_1_3, main2_1_4, main2, main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
+        // (18) main2_1_3(); (19) main2_1_4(); (20) main2();   (queue): [main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4, main2]
+        // (21) main2_1_1(); (22) main2_1_2(); (23) main2_1(); (queue): [main2_1_3, main2_1_4, main2, main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
+        // (24) main2_1_3(); (25) main2_1_4(); (26) main2();   (queue): [main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4, main2]
+        // (27) main2_1_1(); (28) main2_1_2(); (29) main2_1(); (queue): [main2_1_3, main2_1_4, main2, main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
+        // (30) main2_1_3(); (31) main2_1_4(); (32) main2();   (queue): [main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4, main2]
+        // (33) main2_1_1(); (34) main2_1_2(); (35) main2_1(); (queue): [main2_1_3, main2_1_4, main2, main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
+        // (36) main2_1_3(); (37) main2_1_4(); (38) main2();   (queue): [main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4, main2]
+        // (39) main2_1_1(); (40) main2_1_2(); (41) main2_1(); (queue): [main2_1_3, main2_1_4, main2, main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
+        // (42) main2_1_3(); (43) main2_1_4(); (44) main2();   (queue): [main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4, main2]
+        // (45) main2_1_1(); (46) main2_1_2(); (47) main2_1(); (queue): [main2_1_3, main2_1_4, main2, main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
+        // (48) main2_1_3(); (49) main2_1_4(); (50) main2();   (queue): [main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
+        // (51) main2_1_1(); (52) main2_1_2(); (53) main2_1(); (queue): [main2_1_3, main2_1_4, main2_1_1, main2_1_2, main2_1, main2_1_3, main2_1_4]
+        // (54) main2_1_3(); (55) main2_1_4(); (56) main2_1_1(); (57) main2_1_2(); (58) main2_1(); (queue): [main2_1_3, main2_1_4, main2_1_1, main2_1_2, main2_1_3, main2_1_4]
         // (59) main2_1_3(); (60) main2_1_4(); (61) main2_1_1(); (62) main2_1_2(); (63) main2_1_3(); (64) main2_1_4();
         // end execution: main2
         expect(icof1(main2)).toEqual([5, 6, 8, 14, 20, 26, 32, 38, 44, 50]);
