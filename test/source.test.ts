4071412f0be66b7a96a80b00efcaf7a5ec73f928
Anthony Rota <28838817+anthonyrota@users.noreply.github.com>
2020-05-30 20:18:21 +1000

    Add basic source.ts tests

---
+++ test/source.test.ts
@@ -0,0 +1,67 @@
+import { EventType, Push, Throw, End, Source, subscribe } from '../src/source';
+
+describe('EventType', () => {
+    it('should have a Push property equal to zero', () => {
+        expect(EventType.Push).toBe(0);
+    });
+
+    it('should have a Throw property equal to one', () => {
+        expect(EventType.Throw).toBe(1);
+    });
+
+    it('should have an End property equal to two', () => {
+        expect(EventType.End).toBe(2);
+    });
+});
+
+describe('Push', () => {
+    it('should exist', () => {
+        expect(Push).toBeFunction();
+    });
+
+    it('should return a Push event', () => {
+        const value = { foo: 'bar' };
+        const event = Push(value);
+        expect(event).toEqual({ type: EventType.Push, value });
+    });
+});
+
+describe('Throw', () => {
+    it('should exist', () => {
+        expect(Throw).toBeFunction();
+    });
+
+    it('should return a Throw event', () => {
+        const error = { foo: 'bar' };
+        const event = Throw(error);
+        expect(event).toEqual({ type: EventType.Throw, error });
+    });
+});
+
+describe('End', () => {
+    it('should exist', () => {
+        expect(End).toBeObject();
+    });
+
+    it('should be an End event', () => {
+        expect(End).toEqual({ type: EventType.End });
+    });
+});
+
+describe('Source', () => {
+    it('should exist', () => {
+        expect(Source).toBeFunction();
+    });
+
+    it('should return a function', () => {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        const source = Source(() => {});
+        expect(source).toBeFunction();
+    });
+});
+
+describe('subscribe', () => {
+    it('should exist', () => {
+        expect(subscribe).toBeFunction();
+    });
+});
