fd1cecb98264456aa8c5816b9c7744e44f130ad1
Anthony Rota <28838817+anthonyrota@users.noreply.github.com>
2020-05-31 22:21:28 +1000

    Add clarification for subscribe docs

--- src/source.ts
+++ src/source.ts
@@ -127,7 +127,7 @@ export function Source<T>(
  * Higher order function which takes a sink and a subscription, and returns
  * another function which receives a source that will be subscribed to using the
  * given sink and subscription. This is useful, for example, at the end of pipe
- * calls.
+ * calls in order to subscribe to the transformed source.
  * @param sink The sink to be given to the received source.
  * @param subscription The subscription to be given to the received source.
  * @returns The higher order function which takes a source to subscribe to.
