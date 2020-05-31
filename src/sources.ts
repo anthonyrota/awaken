9f6c353faa92356b05d924b9b0db81b0de40dd9c
Anthony Rota <28838817+anthonyrota@users.noreply.github.com>
2020-05-31 22:18:22 +1000

    Add documentation for fromArray/fix type

--- src/sources.ts
+++ src/sources.ts
@@ -1,6 +1,12 @@
 import { Source, Push, End } from './source';
 
-export function fromArray<T>(array: T[]): Source<T> {
+/**
+ * Creates a Source from the given array/array-like. The values of the array
+ * will be synchronously emitted by the created source upon each susbcription.
+ * @param array The array/array-like to take the values from.
+ * @returns The created source.
+ */
+export function fromArray<T>(array: ArrayLike<T>): Source<T> {
     return Source((sink, sub) => {
         for (let i = 0; i < array.length && sub.active; i++) {
             sink(Push(array[i]));
