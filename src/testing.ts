e654ca363a0ae8c6327c8d44952ba584578b8d58
Anthony Rota <28838817+anthonyrota@users.noreply.github.com>
2020-06-01 20:06:14 +1000

    Delete test/testing.test.ts

--- src/testing.ts
+++ src/testing.ts
@@ -17,8 +17,28 @@ export type Unsubscribe = { type: UnsubscribeType };
 export const UnsubscribeType: UnsubscribeType = 5;
 export const Unsubscribe: Unsubscribe = { type: UnsubscribeType };
 
-export type SourceEventType = EventType | DelayType;
-export type SourceEvent<T> = Event<T> | Delay;
+export type TestSourceEventType = EventType | DelayType;
+export type TestSourceEvent<T> = Event<T> | Delay;
 
-export type SubscriptionEventType = DelayType | SubscribeType | UnsubscribeType;
-export type SubscriptionEvent = Delay | Subscribe | Unsubscribe;
+export type TestSubscriptionEventType = DelayType | SubscribeType |UnsubscribeType;
+export type TestSubscriptionEvent =
+    | Delay
+    | Subscribe
+    | Unsubscribe;
+
+export type
+interface TestSource {
+    subscriptions: TestSubscriptions
+}
+
+export function SharedTestSource<T>(...events: Test_SourceEvent<T>): 
+
+export function assertSourceEvents<T>(
+    source: Source<T>,
+    events: Test_SourceEvent<T>,
+): void {}
+
+export function assertSourceSubscriptions(
+    source: Source<T>,
+    events: Test_SubscriptionEvent,
+): void {}
