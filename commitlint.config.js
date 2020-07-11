bfecd3f470dd4ac926bd5041e27984abcf79cbec
anthonyrota <28838817+anthonyrota@users.noreply.github.com>
2020-07-11 16:33:05 +1000

    chore: fix commitlint max line config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,10 @@
+const maxLineLength = 72;
+
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    rules: {
+        'body-max-line-length': [2, 'always', maxLineLength],
+        'footer-max-line-length': [2, 'always', maxLineLength],
+        'header-max-length': [2, 'always', maxLineLength],
+    },
 };
