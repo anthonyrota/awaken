367d980f0b1c0cd5705860bb40381e45ee7184bb
anthonyrota <28838817+anthonyrota@users.noreply.github.com>
2020-09-12 16:53:06 +1000

    fix(core): use strings instead of symbols for nominal identifiers
    This means that the identifiers are the same across environments,
    meaning multiple versions of the library can interop.

--- packages/core/src/symbols.ts
+++ packages/core/src/symbols.ts
@@ -1,17 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function Symbol_(name: string): any {
-    if (typeof Symbol !== 'undefined') {
-        return Symbol(name);
-    }
     // eslint-disable-next-line max-len
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-explicit-any
-    return `__$$_symbol_${name}_$$__` as any;
+    return `__$$_awaken_symbol_${name}_$$__` as any;
 }
 
-const prefix = '@awaken/';
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-export const $$Disposable: unique symbol = Symbol_(prefix + 'Disposable');
+export const $$Disposable: unique symbol = Symbol_('Disposable');
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-export const $$Source: unique symbol = Symbol_(prefix + 'Source');
+export const $$Source: unique symbol = Symbol_('Source');
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-export const $$Sink: unique symbol = Symbol_(prefix + 'Sink');
+export const $$Sink: unique symbol = Symbol_('Sink');
