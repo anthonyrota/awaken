7f3f45a57118cfed4dc973e20ba9c2e0342b03ea
anthonyrota <28838817+anthonyrota@users.noreply.github.com>
2020-10-17 22:50:16 +1100

    save

--- www/src/components/Header.tsx
+++ www/src/components/Header.tsx
@@ -17,9 +17,9 @@ function findIndex<T>(
     list: readonly T[],
     predicate: (value: T) => boolean,
     startIndex = 0,
-    endIndex = list.length,
+    endIndex = list.length - 1,
 ): number {
-    for (let i = startIndex; i < endIndex; i++) {
+    for (let i = startIndex; i <= endIndex; i++) {
         if (predicate(list[i])) {
             return i;
         }
